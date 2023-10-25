21375806372a2520f0df77a9c573c475adc8e130
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-10-25 16:14:30 +0900

    Update build script

--- catkin_ws/BUILD_ROS_WORKSPACE.sh
+++ catkin_ws/BUILD_ROS_WORKSPACE.sh
@@ -1,15 +1,16 @@
 ### Ros Workspace ###
 # Set up the workspace
-source /opt/ros/$ROS_DISTRO/setup.bash
-
 sudo apt update
 
 rosdep update
 
 # Updating ROSDEP and installing dependencies
-cd ~/onolab/catkin_ws && vcs import src < src/.rosinstall
+cd /home/ubuntu/onolab/catkin_ws && vcs import src < src/.rosinstall
 rosdep install --from-paths src --ignore-src --rosdistro=$ROS_DISTRO -y
 
 # Build
 catkin build
+
+# Update enviromental veriables
+source /opt/ros/$ROS_DISTRO/setup.bash
 source /home/ubuntu/onolab/catkin_ws/devel/setup.bash
