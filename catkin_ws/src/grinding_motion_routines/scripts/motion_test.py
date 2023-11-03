47c4fe3f3b118aeb9f6587db0e1c0652beff7c92
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-11-03 07:51:18 +0000

    Update motion routines pkg

--- catkin_ws/src/grinding_motion_routines/scripts/motion_test.py
+++ catkin_ws/src/grinding_motion_routines/scripts/motion_test.py
@@ -2,22 +2,33 @@
 
 import rospy
 from grinding_motion_routines import (
-    moveit_interface,
-    motion_routines,
+    motion_generator,
+    moveit_executor,
+    JTC_executor,
+    motion_primitive,
     marker_display,
     tf_publisher,
+    load_planning_scene,
 )
-import geometry_msgs.msg
 
 
 def main():
     rospy.init_node("mechano_grinding", anonymous=True)
 
-    moveit = moveit_interface.MoveitInterface("manipulator", "pestle_tip")
+    moveit = moveit_executor.MoveitExecutor("arm", "J6")
     current_pose = moveit.move_group.get_current_pose()
 
-    current_pose.pose.position.z -= 0.01
-    moveit.move_PTP(current_pose)
+    current_pose.pose.position.z += 0.03
+    pose_list = [
+        current_pose.pose.position.x,
+        current_pose.pose.position.y,
+        current_pose.pose.position.z,
+        current_pose.pose.orientation.x,
+        current_pose.pose.orientation.y,
+        current_pose.pose.orientation.z,
+        current_pose.pose.orientation.w,
+    ]
+    moveit.execute_to_goal_pose(pose_list)
 
 
 if __name__ == "__main__":
