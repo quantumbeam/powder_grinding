eb43039250870ad033ad9df1f8a2f08d951e688c
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-11-21 12:54:37 +0900

    Add pouring to master sizer

--- catkin_ws/src/grinding_motion_routines/scripts/motion_test.py
+++ catkin_ws/src/grinding_motion_routines/scripts/motion_test.py
@@ -17,24 +17,73 @@ from grinding_motion_routines import (
 )
 
 
+def _pose_stamped_to_list(pose_msg):
+    return [
+        pose_msg.pose.position.x,
+        pose_msg.pose.position.y,
+        pose_msg.pose.position.z,
+        pose_msg.pose.orientation.x,
+        pose_msg.pose.orientation.y,
+        pose_msg.pose.orientation.z,
+        pose_msg.pose.orientation.w,
+    ]
+
+
 def main():
     rospy.init_node("mechano_grinding", anonymous=True)
     debug_tf = tf_publisher.TFPublisher()
     pi = np.pi
 
     mortar_base_position = rospy.get_param("/loading_planning_scene/mortar_position")
+    MasterSizer_position = rospy.get_param(
+        "/loading_planning_scene/MasterSizer_position"
+    )
 
-    moveit = moveit_executor.MoveitExecutor("manipulator", "camera")
-    r = Rotation.from_euler("xyz", [pi, 0, pi / 2], degrees=False)
+    moveit = moveit_executor.MoveitExecutor("manipulator", "spoon_tip")
+    r = Rotation.from_euler("xyz", [pi, pi / 2, pi / 2], degrees=False)
     quat = r.as_quat()
-    mortar_base_pose = list(mortar_base_position.values()) + list(quat)
+    MasterSizer_base_pose = list(MasterSizer_position.values()) + list(quat)
+    pouring_pose = copy.deepcopy(MasterSizer_base_pose)
+    pouring_pose[2] += 0.32
 
-    camera_view_pose = copy.deepcopy(mortar_base_pose)
-    camera_view_pose[2] += 0.2
-    debug_tf.broadcast_tf_with_pose(camera_view_pose, "base_link")
+    tmp_pose = [0.16, 0, 0.35] + list(quat)
 
-    moveit.execute_to_goal_pose(
-        camera_view_pose, ee_link="camera", vel_scale=0.1, acc_scale=0.1, execute=True
+    waypoints = [tmp_pose, pouring_pose]
+    debug_tf.broadcast_tf_with_waypoints(waypoints, "base_link")
+    # moveit.execute_to_goal_pose(
+    #     tmp_pose,
+    #     ee_link="spoon_tip",
+    #     vel_scale=0.2,
+    #     acc_scale=0.2,
+    #     execute=False,
+    # )
+    # moveit.execute_to_goal_pose(
+    #     pouring_pose,
+    #     ee_link="spoon_tip",
+    #     vel_scale=0.1,
+    #     acc_scale=0.1,
+    #     execute=True,
+    # )
+
+    # execute pouring
+    pose_list = []
+    current_pose = _pose_stamped_to_list(
+        moveit.move_group.get_current_pose("spoon_tip")
+    )
+    for i in range(10):
+        delta_rot = Rotation.from_euler("xyz", np.array([0, 0, pi / 3 * 2]) / 10)
+        current_rot = Rotation.from_quat(current_pose[3:])
+        next_rot = current_rot * delta_rot
+        current_position = current_pose[:3]
+        next_pose = current_position + list(next_rot.as_quat())
+        pose_list.append(next_pose)
+        current_pose = next_pose
+    moveit.execute_cartesian_path_by_waypoints(
+        pose_list,
+        "spoon_tip",
+        vel_scale=0.2,
+        acc_scale=0.2,
+        execute=False,
     )
 
 
--- catkin_ws/src/grinding_motion_routines/src/grinding_motion_routines/motion_primitive.py
+++ catkin_ws/src/grinding_motion_routines/src/grinding_motion_routines/motion_primitive.py
@@ -260,3 +260,84 @@ class MotionPrimitive:
             acc_scale=moving_acceleration_scale,
             execute=True,
         )
+
+    def execute_powder_pouring_to_MasterSizer(
+        self,
+        target_position,
+        relay_position=None,
+        delta_euler=np.array([0, 0, pi / 3 * 2]),
+        ee_link="spoon_tip",
+        pouring_velocity_scale=0.5,
+        pouring_acceleration_scale=0.5,
+        moving_velocity_scale=0.2,
+        moving_acceleration_scale=0.2,
+        number_of_motion_steps=10,
+    ):
+        # move to initial pose
+        self.moveit_executor.execute_to_goal_pose(
+            self.init_pose,
+            ee_link="pestle_tip",
+            vel_scale=moving_velocity_scale,
+            acc_scale=moving_acceleration_scale,
+            execute=True,
+        )
+
+        r = Rotation.from_euler("xyz", [pi, pi / 2, pi / 2], degrees=False)
+        quat = r.as_quat()
+
+        # move to relay position
+        if relay_position is not None:
+            relay_pose = list(relay_position) + list(quat)
+            self.moveit_executor.execute_to_goal_pose(
+                relay_pose,
+                ee_link=ee_link,
+                vel_scale=moving_velocity_scale,
+                acc_scale=moving_acceleration_scale,
+                execute=True,
+            )
+
+        # move to pouring ready pose
+        pouring_ready_pose = self._pose_stamped_to_list(
+            self.moveit_executor.move_group.get_current_pose(ee_link)
+        )
+        pouring_ready_pose[0:3] = target_position
+        self.moveit_executor.execute_to_goal_pose(
+            pouring_ready_pose,
+            ee_link=ee_link,
+        )
+
+        # execute pouring
+        pose_list = []
+        current_pose = self._pose_stamped_to_list(
+            self.moveit_executor.move_group.get_current_pose(ee_link)
+        )
+        for i in range(number_of_motion_steps):
+            delta_rot = Rotation.from_euler("xyz", delta_euler / number_of_motion_steps)
+            current_rot = Rotation.from_quat(current_pose[3:])
+            next_rot = current_rot * delta_rot
+            current_position = current_pose[:3]
+            next_pose = current_position + list(next_rot.as_quat())
+            pose_list.append(next_pose)
+            current_pose = next_pose
+        self.moveit_executor.execute_cartesian_path_by_waypoints(
+            pose_list,
+            ee_link,
+            vel_scale=pouring_velocity_scale,
+            acc_scale=pouring_acceleration_scale,
+        )
+
+        # reverse pouring
+        self.moveit_executor.execute_cartesian_path_by_waypoints(
+            reversed(pose_list),
+            ee_link,
+            vel_scale=moving_velocity_scale,
+            acc_scale=moving_acceleration_scale,
+        )
+
+        self.moveit_executor.execute_to_goal_pose(
+            self.init_pose,
+            ee_link="pestle_tip",
+            vel_scale=moving_velocity_scale,
+            acc_scale=moving_acceleration_scale,
+            execute=True,
+        )
