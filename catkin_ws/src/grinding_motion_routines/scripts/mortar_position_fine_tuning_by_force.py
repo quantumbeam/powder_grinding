e5461cfd42527d48b4e47effcf106c5b172ab5e4
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-11-30 09:20:54 +0000

    Update

--- catkin_ws/src/grinding_motion_routines/scripts/mortar_position_fine_tuning_by_force.py
+++ catkin_ws/src/grinding_motion_routines/scripts/mortar_position_fine_tuning_by_force.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
 # ROS-related imports
-import rospy
-import tf.transformations as tf
+
+from scipy.spatial.transform import Rotation
 from threading import Event
+import rospy
 import tf2_ros
 
 from std_srvs.srv import Empty
 from geometry_msgs.msg import WrenchStamped
 from grinding_motion_routines.motion_generator import MotionGenerator
 from grinding_motion_routines.moveit_executor import MoveitExecutor
+from grinding_motion_routines.load_planning_scene import PlanningScene
+from grinding_motion_routines.marker_display import MarkerDisplay
+
 from grinding_motion_routines.srv import PositionCalibrateVector
 
 import argparse
@@ -24,8 +28,12 @@ class MortarPositionFineTuning:
         move_group_name="manipulator",
         ee_link="pestle_tip",
     ):
+        self.marker_display = MarkerDisplay("calibration_marker_array")
+        # Initialize MoveitExecutor
         self.moveit = MoveitExecutor(move_group_name, ee_link)
         self.grinding_eef_link = rospy.get_param("~grinding_eef_link")
+
+        self.scene = PlanningScene(self.moveit.move_group)
 
         self.mortar_position = rospy.get_param("~mortar_top_position")
         self.mortar_scale = rospy.get_param("~mortar_inner_scale")
@@ -46,7 +54,7 @@ class MortarPositionFineTuning:
         rospy.wait_for_service(self.zero_wrench_service_name)
         rospy.wait_for_service("/start_stop_recording")
         try:
-            self.service_client = rospy.ServiceProxy(
+            self.xy_calibrate_service_client = rospy.ServiceProxy(
                 "/start_stop_recording", PositionCalibrateVector
             )
             self.ft_service = rospy.ServiceProxy(self.zero_wrench_service_name, Empty)
@@ -93,22 +101,6 @@ class MortarPositionFineTuning:
             print("tf listen error%s" % err)
             return err
 
-    def move_to_position_above_mortar(self):
-        rospy.loginfo("Moving to start position")
-        q = tf.quaternion_from_euler(pi, 0, pi)
-        pose = [
-            self.mortar_position["x"],
-            self.mortar_position["y"],
-            self.mortar_position["z"] + 0.1,
-        ] + list(q)
-        self.moveit.execute_cartesian_path_to_goal_pose(
-            pose,
-            ee_link=self.grinding_eef_link,
-            vel_scale=0.1,
-            acc_scale=0.1,
-        )
-        self._zero_ft_sensor()
-
     def _move_and_check_force(self, delta):
         current_pose = self._pose_stumped_to_list(
             self.moveit.move_group.get_current_pose()
@@ -139,13 +131,31 @@ class MortarPositionFineTuning:
         print(vertical_pose)
         self.moveit.execute_to_goal_pose(vertical_pose, ee_link=self.grinding_eef_link)
 
+    def move_to_position_above_mortar(self):
+        rospy.loginfo("Moving to start position")
+        q = Rotation.from_euler("xyz", [0, pi, 0]).as_quat()
+        pose = [
+            self.mortar_position["x"],
+            self.mortar_position["y"],
+            self.mortar_position["z"] + 0.05,
+        ] + list(q)
+        self.moveit.execute_cartesian_path_to_goal_pose(
+            pose,
+            ee_link=self.grinding_eef_link,
+            vel_scale=0.1,
+            acc_scale=0.1,
+        )
+
+        self._zero_ft_sensor()
+
     def caliblate_pestle_tip_position(self, boad_tickness):
         self._move_verticale_pose_of_current_position()
         self._zero_ft_sensor()
 
         delta = np.zeros(7)
+        step = rospy.get_param("~Z_calibration_step")
         for _ in range(1000):
-            delta[2] = -0.001
+            delta[2] = -step
             result = self._move_and_check_force(delta)
             if result:
                 rospy.loginfo("Calibration finished")
@@ -171,15 +181,18 @@ class MortarPositionFineTuning:
         self._zero_ft_sensor()
 
         delta = np.zeros(7)
-
+        step = rospy.get_param("~Z_calibration_step")
         for _ in range(1000):
-            delta[2] = -0.001
+            delta[2] = -step
             result = self._move_and_check_force(delta)
             if result:
                 rospy.loginfo("Calibration finished")
                 pestle_tip_pose = self.moveit.move_group.get_current_pose()
                 mortar_position_Z = pestle_tip_pose.pose.position.z - boad_tickness
                 rospy.loginfo("Mortar position Z: %s", mortar_position_Z)
+                self.mortar_position["z"] = mortar_position_Z
+                rospy.set_param("~mortar_top_position", self.mortar_position)
+                self.scene.init_planning_scene()
                 break
             if rospy.is_shutdown():
                 break
@@ -187,73 +200,77 @@ class MortarPositionFineTuning:
     def set_rough_mortar_position(self):
         self._move_verticale_pose_of_current_position()
         self.rough_mortar_position = self.moveit.move_group.get_current_pose()
+
         rospy.loginfo(f"Rough mortar position: {self.rough_mortar_position}")
+        self.mortar_position["x"] = self.rough_mortar_position.pose.position.x
+        self.mortar_position["y"] = self.rough_mortar_position.pose.position.y
+        rospy.set_param("~mortar_top_position", self.mortar_position)
+        self.scene.init_planning_scene()
 
     def fine_tuning_mortar_position(self):
-        generator = MotionGenerator()
+        variance_threshold = rospy.get_param("~calibrated_variance_of_force")
+
         self.move_to_position_above_mortar()
+        generator = MotionGenerator(self.mortar_position, self.mortar_scale)
 
-        for i in range(10):
-            waypoints = generator.create_circular_waypoints(
-                begining_position=rospy.get_param("~grinding_pos_begining"),
-                end_position=rospy.get_param("~grinding_pos_end"),
-                begining_radious_z=rospy.get_param("~grinding_rz_begining"),
-                end_radious_z=rospy.get_param("~grinding_rz_end"),
-                angle_param=rospy.get_param("~grinding_angle_param"),
-                yaw_angle=rospy.get_param("~grinding_yaw_angle"),
-                number_of_rotations=rospy.get_param("~grinding_number_of_rotation"),
-                number_of_waypoints_per_circle=rospy.get_param(
-                    "~grinding_number_of_waypoints_per_circle"
-                ),
-                center_position=rospy.get_param("~grinding_center_pos"),
-                debug=False,
-                format="list",
-            )
+        # Create waypoints
+        waypoints = generator.create_circular_waypoints(
+            begining_position=rospy.get_param("~grinding_pos_begining"),
+            end_position=rospy.get_param("~grinding_pos_end"),
+            begining_radious_z=rospy.get_param("~grinding_rz_begining"),
+            end_radious_z=rospy.get_param("~grinding_rz_end"),
+            angle_param=rospy.get_param("~grinding_angle_param"),
+            yaw_angle=rospy.get_param("~grinding_yaw_angle"),
+            number_of_rotations=rospy.get_param("~grinding_number_of_rotation"),
+            number_of_waypoints_per_circle=rospy.get_param(
+                "~grinding_number_of_waypoints_per_circle"
+            ),
+        )
 
-            # Inverse kinematics
-            # 収束判定つき、ジョイントリミットを超えていたら再計算
-            isConverged = False
-            while not isConverged:
-                joint_trajectory = []
-                for pose in waypoints:
-                    joints = self._solve_ik(pose)
+        self.marker_display.display_waypoints(waypoints, clear=True)
 
-                    if joints == "ik_not_found":
-                        rospy.logerr("IK not found")
-                    else:
-                        joint_trajectory.append(joints)
-                joints_difference = np.max(joint_trajectory, axis=0) - np.min(
-                    joint_trajectory, axis=0
-                )
-                sum_joints_difference = np.sum(joints_difference)
-                if sum_joints_difference > self.sum_joints_limits_for_trajectory:
-                    rospy.logerr("Trajectory is out of joints limits -> recalculating")
-                    rospy.logerr(f"Sum of joints difference: {sum_joints_difference}")
-                else:
-                    rospy.loginfo(f"Trajectory is in joints limits:{joints_difference}")
-                    isConverged = True
+        # Execute waypoints
+        rospy.loginfo("Goto initial position")
+        self.moveit.execute_cartesian_path_to_goal_pose(
+            waypoints[0], ee_link=self.grinding_eef_link, vel_scale=0.1, acc_scale=0.1
+        )
+        rospy.loginfo("Start calibration grinding motion")
+        self.xy_calibrate_service_client("start")
+        self.moveit.execute_cartesian_path_by_waypoints(
+            waypoints, ee_link=self.grinding_eef_link, vel_scale=1, acc_scale=1
+        )
+        response = self.xy_calibrate_service_client("stop")
+        self.move_to_position_above_mortar()
+        rospy.loginfo("Calibration grinding motion finished")
 
-            self.set_joint_positions(joint_trajectory[0], t=3, wait=True)
-            self.service_client("start")
-            self.set_joint_trajectory(joint_trajectory, t=5)
-            response = self.service_client("stop")
+        # Update mortar position
+        # direction of x axis of end-effector is opposite from base_link
+        # direction of y axis of end-effector is same as base_link
+        step = rospy.get_param("~XY_calibration_step")
+        new_mortar_position = self.mortar_position.copy()
+        if response.integral_force_x > 0:
+            new_mortar_position["x"] += step
+        elif response.integral_force_x < 0:
+            new_mortar_position["x"] -= step
+        if response.integral_force_y > 0:
+            new_mortar_position["y"] -= step
+        elif response.integral_force_y < 0:
+            new_mortar_position["y"] += step
+        self.mortar_position = new_mortar_position
 
-            # Move to start position
-            self.move_to_position_above_mortar()
+        rospy.loginfo("Current mortar position: %s", self.mortar_position)
+        rospy.loginfo("New mortar position: %s", self.mortar_position)
 
-            # Update mortar position
-            mortar_position = generator.mortar_center_position
-            if response.integral_force_x > 0:
-                mortar_position.x -= self.calibration_step
-            elif response.integral_force_x < 0:
-                mortar_position.x += self.calibration_step
+        # rospy.set_param("~mortar_top_position", self.mortar_position)
+        # self.scene.init_planning_scene()
+        # generator.update_mortar_position(self.mortar_position)
 
-            if response.integral_force_y > 0:
-                mortar_position.y -= self.calibration_step
-            elif response.integral_force_y < 0:
-                mortar_position.y += self.calibration_step
-            generator.update_mortar_position(mortar_position)
-            rospy.loginfo(f"New mortar position: {mortar_position}")
+        # Check variance of force
+        if (
+            abs(response.integral_force_x) < variance_threshold
+            and abs(response.integral_force_y) < variance_threshold
+        ):
+            rospy.loginfo("Calibration finished")
 
     def subscribe_wrench(self):
         rospy.loginfo("Subscribing to wrench for 10 seconds")
@@ -279,6 +296,9 @@ class MortarPositionFineTuning:
         self.timer_event.set()
         rospy.loginfo("Stopping wrench subscription after 10 seconds")
 
+    def initialize_scene(self):
+        self.scene.init_planning_scene()
+
 
 def main():
     rospy.init_node("mortar_position_calibration", anonymous=True)
@@ -295,6 +315,7 @@ def main():
             + "4: Fine tuning Mortar XY position\n"
             + "5: Calibrate grinding target force\n"
             + "6: Subscribe wrench(debug)\n"
+            + "7: Initialize scene\n"
         )
         if key == "0":
             rospy.loginfo("Exit calibration mode")
@@ -321,6 +342,8 @@ def main():
             arm.caliblate_grinding_motion_to_target_force()
         elif key == "6":
             arm.subscribe_wrench()
+        elif key == "7":
+            arm.initialize_scene()
 
 
 if __name__ == "__main__":
