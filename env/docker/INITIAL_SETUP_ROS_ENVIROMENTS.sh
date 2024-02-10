4c060c3bf82e795854705023e3b463ed04d37fc9
Yusaku Nakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2024-02-10 11:22:43 +0900

    Add copy cobotta setup files

--- env/docker/INITIAL_SETUP_ROS_ENVIROMENTS.sh
+++ env/docker/INITIAL_SETUP_ROS_ENVIROMENTS.sh
@@ -19,3 +19,11 @@ catkin build
 # Update enviromental veriables
 source /opt/ros/$ROS_DISTRO/setup.bash
 source devel/setup.bash
+
+
+# Copy description files to DENSO ROS package
+rosrun denso_robot_bringup install_robot_description.py ./src/powder_grinding/grinding_descriptions/urdf/cobotta/setup_files/cobotta_description
+rosrun denso_robot_bringup update_joint_limits.py cobotta ./src/powder_grinding/grinding_descriptions/urdf/cobotta/setup_files/joint_limits.yaml
+echo "It is fine if cobotta's setup_files does not exist even if you run this script more than once."
+
+echp "If you can't use roslauch, please run the 'source devel/setup.bash' on the terminal."
