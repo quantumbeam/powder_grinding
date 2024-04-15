9a12210e84318bd68ca2cac875cfd7c5e19201ce
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2024-04-16 07:34:52 +0900

    Rearange denso robot descriptions

--- env/docker/INITIAL_SETUP_ROS_ENVIROMENTS.sh
+++ env/docker/INITIAL_SETUP_ROS_ENVIROMENTS.sh
@@ -23,7 +23,8 @@ source devel/setup.bash
 
 # Copy description files to DENSO ROS package
 rosrun denso_robot_bringup install_robot_description.py ./src/powder_grinding/grinding_descriptions/urdf/cobotta/setup_files/cobotta_description
-rosrun denso_robot_bringup install_robot_description.py ./src/powder_grinding/grinding_descriptions/urdf/cobotta_pro/cobotta_pro_900_description
+rosrun denso_robot_bringup install_robot_description.py ./src/powder_grinding/grinding_descriptions/urdf/cobotta_pro/setup_files/cobotta_pro_900_description
 rosrun denso_robot_bringup update_joint_limits.py cobotta ./src/powder_grinding/grinding_descriptions/urdf/cobotta/setup_files/joint_limits.yaml
+# rosrun denso_robot_bringup update_joint_limits.py cobotta_pro_900 ./src/powder_grinding/grinding_descriptions/urdf/cobotta_pro/setup_files/joint_limits.yaml
 echo "It is fine if cobotta's setup_files does not exist even if you run this script more than once."
 echp "If you can't use roslauch, please run the 'source devel/setup.bash' on the terminal."
