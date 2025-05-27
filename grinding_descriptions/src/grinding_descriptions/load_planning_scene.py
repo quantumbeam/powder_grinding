29dd80dcc9002900421f584a3637398b0bc253b5
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-05-27 12:00:36 +0900

    Update scene

--- grinding_descriptions/src/grinding_descriptions/load_planning_scene.py
+++ grinding_descriptions/src/grinding_descriptions/load_planning_scene.py
@@ -1,46 +1,70 @@
 #!/usr/bin/env python3
-import sys
 import rospy
 import roslib
 import geometry_msgs.msg
 from moveit_commander import PlanningSceneInterface
 from moveit_commander import MoveGroupCommander
-from moveit_commander import roscpp_initialize, roscpp_shutdown
 
 
 class PlanningScene:
+    """
+    A class to manage the planning scene for a MoveIt! move group.
+
+    Attributes:
+        move_group (MoveGroupCommander): The move group to manage the planning scene for.
+        planning_frame (str): The planning frame for the move group.
+        scene (PlanningSceneInterface): The interface to the planning scene.
+    """
+
     def __init__(self, move_group):
-        self.move_group = move_group
-        self.planning_frame = move_group.get_planning_frame()
+        """
+        The constructor for PlanningScene class.
+
+        Parameters:
+            move_group (str or MoveGroupCommander): The name of the move group or a MoveGroupCommander instance.
+        """
+        if isinstance(move_group, MoveGroupCommander):
+            self.move_group = move_group
+        elif isinstance(move_group, str):
+            self.move_group = MoveGroupCommander(move_group)
+        self.planning_frame = self.move_group.get_planning_frame()
         self.scene = PlanningSceneInterface()
-        self.scene.remove_world_object("table")
-        self.scene.remove_world_object("mortar")
-        self.scene.remove_world_object("funnel")
-       
+        self.scene.remove_world_object("")
+
     def init_planning_scene(self):
-        mortar_mesh_file_path = (
-            roslib.packages.get_pkg_dir("grinding_descriptions")
-            + "/mesh/moveit_scene_object/mortar_40mm.stl"
-        )
+        """
+        Initializes the planning scene by adding objects to it based on ROS parameters.
+        """
         table_pos = rospy.get_param("~table_position", None)
         table_scale = rospy.get_param("~table_scale", None)
-        mortar_pos = rospy.get_param("~mortar_top_position", None)
-        funnel_pos = rospy.get_param("~funnel_position", None)
-        funnel_scale = rospy.get_param("~funnel_scale", None)
-    
-        # Add table if parameters are provided
         if table_pos and table_scale:
             self._add_table(table_scale, table_pos)
+        else:
+            rospy.logwarn("Table parameters not provided")
 
-        # Add mortar if parameters are provided
-        if mortar_pos:
+        mortar_pos = rospy.get_param("~mortar_top_position", None)
+        mortar_mesh_file_name = rospy.get_param("~mortar_mesh_file_name", None)
+        if mortar_pos and mortar_mesh_file_name:
+            mortar_mesh_file_path = (
+                roslib.packages.get_pkg_dir("grinding_descriptions")
+                + "/mesh/moveit_scene_object/"
+                + mortar_mesh_file_name
+                + ".stl"
+            )
+            rospy.loginfo(f"Adding mortar from {mortar_mesh_file_path}")
             self._add_mortar(mortar_mesh_file_path, mortar_pos)
+        else:
+            rospy.logwarn("Mortar parameters not provided")
 
-        # Add funnel if parameters are provided
-        if funnel_pos and funnel_scale:
-            self._add_funnel(funnel_pos, funnel_scale)
 
     def _add_table(self, table_scale, table_pos):
+        """
+        Adds a table to the planning scene.
+
+        Parameters:
+            table_scale (dict): The scale of the table.
+            table_pos (dict): The position of the table.
+        """
         table_pose = geometry_msgs.msg.PoseStamped()
         table_pose.header.frame_id = self.planning_frame
         table_pose.pose.orientation.w = 1.0
@@ -53,6 +77,13 @@ class PlanningScene:
         )
 
     def _add_mortar(self, file_path, mortar_pos):
+        """
+        Adds a mortar to the planning scene.
+
+        Parameters:
+            file_path (str): The path to the mesh file for the mortar.
+            mortar_pos (dict): The position of the mortar.
+        """
         mortar_pose = geometry_msgs.msg.PoseStamped()
         mortar_pose.header.frame_id = self.planning_frame
         mortar_pose.pose.position.x = mortar_pos["x"]
@@ -60,25 +91,9 @@ class PlanningScene:
         mortar_pose.pose.position.z = mortar_pos["z"]
         self.scene.add_mesh("Mortar", mortar_pose, file_path)
 
-    def _add_funnel(self, funnel_pos, funnel_scale):
-        funnel_pose = geometry_msgs.msg.PoseStamped()
-        funnel_pose.header.frame_id = self.planning_frame
-        funnel_pose.pose.orientation.w = 1.0
-        funnel_pose.pose.position.x = funnel_pos["x"]
-        funnel_pose.pose.position.y = funnel_pos["y"]
-        funnel_pose.pose.position.z = funnel_scale["z"] / 2
-        self.scene.add_cylinder(
-            "Funnel",
-            funnel_pose,
-            funnel_scale["z"],
-            funnel_scale["x"],
-        )
 
-    
 if __name__ == "__main__":
-    roscpp_initialize(sys.argv)
     rospy.init_node("load_planning_scene")
-    move_group = MoveGroupCommander(rospy.get_param("~move_group_name", None))
-    planning_scene = PlanningScene(move_group)
+    move_group_name = rospy.get_param("~move_group_name")
+    planning_scene = PlanningScene(MoveGroupCommander(move_group_name))
     planning_scene.init_planning_scene()
-    roscpp_shutdown()
